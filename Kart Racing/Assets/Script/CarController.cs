d6bc34c53ada0d39c67e1107b3d50ae5bd87902d
erikokres <erikokresna@gmail.com>
2023-01-05 08:19:25 +0700

    Add Main

--- Kart Racing/Assets/Script/CarController.cs
+++ Kart Racing/Assets/Script/CarController.cs
@@ -10,13 +10,23 @@ public class CarController : MonoBehaviour
     public float forwardAccel = 8f, reverseAccel = 4f;
     public float turnStrength = 100f;
 
+    public Transform groundRayPoint, groundRayPoint2;
+    public LayerMask whatIsGround;
+    public float groundRayLength = 0.75f;
+    public float gravityMod =10f;
+
     private float speedInput;
     private float turnInput;
+    private bool grounded;
+    private float dragOnGround;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb.transform.parent = null;
+
+        dragOnGround = rb.drag;
     }
 
     // Update is called once per frame
@@ -32,7 +42,7 @@ public class CarController : MonoBehaviour
              speedInput = Input.GetAxis("Vertical") * reverseAccel;
         }
         turnInput = Input.GetAxis("Horizontal");
-        if (Input.GetAxis("Vertical") != 0)
+        if (grounded && Input.GetAxis("Vertical") != 0)
         {
             transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f,turnInput * turnStrength 
             * Time.deltaTime * Mathf.Sign(speedInput) * (rb.velocity.magnitude/maxSpeed) ,0f));
@@ -44,12 +54,46 @@ public class CarController : MonoBehaviour
     
     private void FixedUpdate() 
     {
-        
-        rb.AddForce(transform.forward * speedInput * 500f);
 
-        if(rb.velocity.magnitude > maxSpeed){
+        grounded = false;
+
+        RaycastHit hit;
+        Vector3 normalTarget = Vector3.zero;
+        if (Physics.Raycast(groundRayPoint.position, -transform.up, out hit, groundRayLength, whatIsGround))
+        {
+            grounded = true;
+
+            normalTarget = hit.normal;
+        }
+        //when on ground rotate to the normal
+        if(grounded){
+            transform.rotation = Quaternion.FromToRotation(transform.up, normalTarget)*transform.rotation;
+        }
+
+        if(Physics.Raycast(groundRayPoint2.position, -transform.up, out hit, groundRayLength, whatIsGround)){
+            grounded = true;
+
+            normalTarget = (normalTarget + hit.normal) /2f;
+        }
+        
+
+        //accelerate the car
+        if(grounded)
+        {
+            rb.AddForce(transform.forward * speedInput * 500f);
+            rb.drag = dragOnGround;
+        }else
+        {
+            rb.drag = 0.1f;
+
+            rb.AddForce(-Vector3.up * gravityMod *100f);
+        }
+
+        if(rb.velocity.magnitude > maxSpeed)
+        {
             rb.velocity = rb.velocity.normalized * maxSpeed;
         }
-        Debug.Log(rb.velocity.magnitude);
+
+        
     }
 }
